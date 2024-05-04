a1bbbecceee2aea889fb942f90afc8ca01ad4d63
slivanovich <svyatoslav.sk4@gmail.com>
2024-05-04 22:26:26 +0300

    Fullfil cuda events

--- src/main/cpp/cuda/CudaEvent.cuh
+++ src/main/cpp/cuda/CudaEvent.cuh
@@ -13,15 +13,27 @@
 
 /* CUDA event class */
 class CudaEvent {
-  std::string NameOfEvent;
-  std::vector<std::pair<int, DWORD *>> DeviceBuffers;
+  std::string NameOfEvent; // Name of the current CUDA event
+  std::vector<std::pair<int, DWORD *>> DeviceBuffers; // Device buffers for CUDA
   std::map<std::string, int>
-      OffsetsForDeviceBuffer;
+      OffsetsForDeviceBuffer; // Offsets in CUDA device buffers for response
+                              // function
   std::function<void(std::vector<std::pair<int, DWORD *>>,
                      std::map<std::string, int>)>
-      Response;
-  bool IsTrue;
+      Response; // Response function for CUDA calculating
+  bool IsTrue;  // If this flag is false there was some mistake in current CUDA
+                // event
 
+  /* Class constructor function.
+   * ARGUMENTS:
+   *   - name of CUDA event:
+   *       const std::string &name;
+   *   - size of device buffers for CUDA:
+   *       const std::vector<int> &BufSize;
+   *   - response function for CUDA:
+   *       const std::function<void(std::vector<std::pair<int, DWORD *>>,
+   * std::map<std::string, int>)> &nF;
+   */
   explicit CudaEvent(
       const std::string &Name, const std::vector<int> &BufSize,
       const std::function<void(std::vector<std::pair<int, DWORD *>>,
@@ -46,7 +58,72 @@ class CudaEvent {
     IsTrue = true;
   } /* End of 'CudaEvent' function */
 
+public:
+  /* Get event name function.
+   * ARGUMENTS: None.
+   * RETURNS:
+   *   (const std::string &) - name of the event.
+   */
+  const std::string &GetEventName() {
+    return NameOfEvent;
+  } /* End of 'GetEventName' function */
+
+  void SetOffset(const std::string &FieldName, const int Value) {
+    OffsetsForDeviceBuffer[FieldName] = Value;
+  } /* End of 'SetOffset' function */
+
+  /* Release CUDA event function.
+   * ARGUMENTS:
+   *   - offsets in buffers:
+   *       const std::map<std::string, int> &Src;
+   * RETURNS: None.
+   */
+  void ReleaseEvent(std::vector<DWORD *> &HostBuffers) {
+    if (HostBuffers.size() != DeviceBuffers.size())
+      std::cout << "Sizes of host and device buffers are not equal\n",
+          IsTrue = false;
+    if (IsTrue) {
+      CopyHostData(HostBuffers);
+      Response(DeviceBuffers, OffsetsForDeviceBuffer);
+      CopyDeviceData(HostBuffers);
+    } else
+      CloseEvent();
+  } /* End of 'ReleaseEvent' function */
+
+  /* Change response function.
+   * ARGUMENTS:
+   *   - new response function:
+   *       const std::function<void(std::vector<std::pair<int, DWORD *>>,
+   * std::map<std::string, int>)> &nF; RETURNS: None.
+   */
+  void ChangeResponseFunction(
+      const std::function<void(std::vector<std::pair<int, DWORD *>>,
+                               std::map<std::string, int>)> &nF) {
+    Response = nF;
+  } /* End of 'ChangeResponseFunction' function */
+
+  /* Resize certain device buffer function.
+   * ARGUMENTS:
+   *   - buffer index:
+   *       const int BufferIndex;
+   * RETURNS: None.
+   */
+  void ResizeBuffer(const int BufferIndex) {
+    if (DeviceBuffers[BufferIndex].second != nullptr)
+      cudaFree(DeviceBuffers[BufferIndex].second);
+    if (cudaMalloc((void **)&DeviceBuffers[BufferIndex].second,
+                   DeviceBuffers[BufferIndex].first * sizeof(DWORD)) !=
+        cudaSuccess)
+      std::cout << "cudaMalloc failed!\n", IsTrue = false;
+  } /* End of 'ResizeBuffer' function */
+
 private:
+  /* Copy data from device to host buffers function.
+   * ARGUMENTS:
+   *   - host buffers vector:
+   *       std::vector<DWORD *> &HostBuffers;
+   * RETURNS: None.
+   */
   void CopyDeviceData(std::vector<DWORD *> &HostBuffers) {
     cudaError_t Status;
     for (int i = 0; i < HostBuffers.size(); i++) {
@@ -61,6 +138,10 @@ private:
     }
   } /* End of 'CopyDeviceData' function */
 
+  /* Copy data from host to device buffers function.
+   * ARGUMENTS: None.
+   * RETURNS: None.
+   */
   void CopyHostData(const std::vector<DWORD *> &HostBuffers) {
     cudaError_t Status;
     for (int i = 0; i < DeviceBuffers.size(); i++) {
@@ -75,6 +156,21 @@ private:
     }
   } /* End of 'CopyDeviceData' function */
 
+  /* Close event function.
+   * ARGUMENTS: None.
+   * RETURNS: None.
+   */
+  void CloseEvent() {
+    if (cudaDeviceReset() != cudaSuccess)
+      std::cout << "cudaDeviceReset failed!\n";
+    if (!DeviceBuffers.empty())
+      for (auto &b : DeviceBuffers)
+        if (b.second != nullptr)
+          cudaFree(b.second);
+  } /* End of 'CloseEvent' function */
+
+  /* Class destructor function */
+  ~CudaEvent() { CloseEvent(); } /* End of '~CudaEvent' function */
 }; /* End of 'CudaEvent' class */
 
 #endif /* __cuda_cuh_ */
